d57de23e0f9bf42edfadb75eef86b61ddd3cd7f5
ber34 <ber34@o2.pl>
2013-08-14 23:53:23 +0200

    Update meta.php

--- template/meta.php
+++ template/meta.php
@@ -10,3 +10,6 @@ $title= "dfg";
     <link type="text/css" href="template/css/my_chat.css" rel="stylesheet" />
     <script src="http://code.jquery.com/jquery-1.9.1.min.js"></script>
     <script src="http://code.jquery.com/jquery-migrate-1.1.1.min.js"></script>
+ <!--[if lt IE 9]>
+<script src="http://html5shim.googlecode.com/svn/trunk/html5.js"></script>
+<![endif]-->
