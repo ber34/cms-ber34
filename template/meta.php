b7c785f107858ae232005fb23d4ab807add398f9
ber34 <ber34@o2.pl>
2013-08-14 23:48:11 +0200

    Create meta.php

---
+++ template/meta.php
@@ -0,0 +1,12 @@
+<?php
+$title= "dfg"; 
+       ?>
+    <title><?php echo $title; ?></title>
+    <meta charset="UTF-8">
+    <meta name="keywords" content="<?php echo $keywords; ?>"/>
+    <meta name="Description" content="<?php echo $description; ?>"/>
+    <meta name="robots" content="index, follow" />
+    <link type="text/css" href="template/css/style.css" rel="stylesheet" />
+    <link type="text/css" href="template/css/my_chat.css" rel="stylesheet" />
+    <script src="http://code.jquery.com/jquery-1.9.1.min.js"></script>
+    <script src="http://code.jquery.com/jquery-migrate-1.1.1.min.js"></script>
