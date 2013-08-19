72217b2f6ec29c9e7f020bfcdd66f234adaa60d7
ber34 <ber34@o2.pl>
2013-08-19 07:26:13 +0200

    Create admin_menu.php

---
+++ admin/admin_menu.php
@@ -0,0 +1,8 @@
+<?php
+?>
+           <div>
+             <span><a href="<?php echo htmlentities(addslashes('index.php?admin=index'),ENT_QUOTES,'UTF-8'); ?>" target="">Start</a></span>
+	     <span><a href="<?php echo htmlentities(addslashes('index.php?admin=admin_user'),ENT_QUOTES,'UTF-8'); ?>" target="">User</a></span>
+	     <span><a href="<?php echo htmlentities(addslashes('index.php?admin=chatadmin'),ENT_QUOTES,'UTF-8'); ?>" target="">Chat</a></span>
+             <span><a href="<?php echo htmlentities(addslashes('index.php?admin=wyloguj'),ENT_QUOTES,'UTF-8'); ?>" target="">Wyloguj</a></span>
+           </div>
