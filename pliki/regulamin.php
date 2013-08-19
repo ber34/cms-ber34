59d4779cfeb4d457d5e977aedff1a0b3b65e3d76
ber34 <ber34@o2.pl>
2013-08-19 07:21:30 +0200

    Create regulamin.php

---
+++ pliki/regulamin.php
@@ -0,0 +1,52 @@
+<div align="left">
+  <ul style="list-style: none;">
+  <h1><b style="color:red;">Regulamin</b></h1>
+  <h2><b>http://www.bearsurf.com.pl</b></h2>
+   <li><h4><b style="color:red;">1. </b>Korzystając z Serwisu <b> http://www.bearsurf.com.pl</b>
+         użytkownik ma obowiązek zapoznać się z treścią poniższego regulaminu, <br />
+         oraz akceptuje jego postanowienia i zobowiązuje się do ich przestrzegania. <br /><br />
+	   </h4>
+   </li>
+       <li><font color="red"><h4><b>2.</b></font> Nie wolno dodawać stron, banerów i aukcji erotycznych, hazardowych, propagujących nienawiść i przemoc, <br />
+    niezgodnych z prawem polskim, obrażające osoby trzecie lub też zawierających linki do stron o takiej tematyce. <br /><br /></h4></li>
+
+   <li><font color="red"><h4><b>3.</b></font> Każdy użytkownik, może mieć tylko jedno konto. <br /><br /></h4></li>
+
+   <li><font color="red"><h4><b> 4.</b></font> Administrator może usunąć stronę bez podania przyczyny ! <br /><br /></h4></li>
+
+  <li><font color="red"><h4><b> 5.</b></font> Administrator może dokonać zmian wprowadzonych przez Ciebie informacji,<br />
+   tak, by były poprawne i zgodne z wymogami www.bearsurf.com.pl<br /><br /></h4></li>
+
+    <li><font color="red"><h4><b>6.</b></font> Akceptacja niniejszego regulaminu przez użytkownika jest równoznaczna z oświadczeniem:<br />
+    "Wyrażam zgodę na przetwarzanie i wykorzystywanie moich danych osobowych,<br />
+      wprowadzonych do formularzy dostępnych na stronach serwisu,<br />
+      w celach marketingowych oraz umieszczenie ich w bazie danych firmy: <br />
+      www.bearsurf.com.pl" oraz "Wyrażam zgodę na otrzymywanie informacji handlowych od firmy www.bearsurf.com.pl<br />
+      w rozumieniu ustawy z dnia 18 lipca 2002 roku o świadczeniu usług drogą elektroniczną". <br /><br /></h4></li>
+
+   <li><font color="red"><h4><b>7.</b></font> Serwis www.bearsurf.com.pl zastrzega sobie prawo
+   do dokonywania zmian i modyfikacji niniejszego regulaminu.<br />
+     Zmiany regulaminu będą udostępniane użytkownikom na stronach internetowych serwisu. <br /><br /></h4></li>
+	 
+	<li><font color="red"><h4><b>8.</b></font>Korzystając z www.bearsurf.com.pl możesz dodać dowolną ilość stron, banerów i aukcji Allegro. <br /><br /></h4></li>
+	
+	<li><font color="red"><h4><b>9.</b></font>Wyświetlane Strony w www.bearsurf.com.pl nie mogą posiadać popupów <br />
+	hamujących rotację wyświetlanych stron, a także innych elementów na takie zachowanie wpływające.<br />
+     Strony naruszające tę część regulaminu będą usuwane ze wszystkimi punktami.  
+	<br /><br /></h4></li>
+	
+   <li><font color="red"><h4><b>10.</b></font>Zabronione jest dodawanie stron, której zawartość jest niezgodna z polskim prawem,
+   a także z jakimikolwiek wirusami. <br /><br /></h4></li>
+	    
+    <li><font color="red"><h4><b>11.</b></font>Zamieszczone strony w www.bearsurf.com.pl nie mogą wpływać,<br />
+	na działanie skryptu automatycznego wyświetlania stron,<br />. 
+	ani w żaden sposób wpływać na jego błędne działanie<br />
+	 </h4><br /></li>
+	 
+	  <li><font color="red"><h4><b>12.</b></font> Serwis www.bearsurf.com.pl jak i administracja nie odpowiada za<br />
+	  treść ston zamieszczonych przez użytkowników. <br /><br /></h4></li> 
+	  
+	  <li><font color="red"><h4><b>13.</b></font>Serwis www.bearsurf.com.pl jak i administracja zastrzega sobie<br />
+	  prawo, do niezaakceptowania stron, bez podania przyczyny.<br /><br /></h4></li>	
+   
+    </ul></div>                          
