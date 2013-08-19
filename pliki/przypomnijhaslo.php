cb179151ed79670023ffbabcab3686c4d7d39369
ber34 <ber34@o2.pl>
2013-08-19 07:20:10 +0200

    Create przypomnijhaslo.php

---
+++ pliki/przypomnijhaslo.php
@@ -0,0 +1,12 @@
+<?php
+                   
+?>
+	<header align="center" class="przyp_yello1">
+	<form action="index.php" method="post">
+	<fieldset>
+	 <legend style="color: red;">Przypomnij hasło:</legend>
+      <span class="submit_from">E-mail :<input class="msgInfo" name="login" type="text" placeholder="E-mail wyslij"></span>
+      <span><input class="przyp_submit" name="" type="submit" value="Wyślij" /></span>
+	 </fieldset>
+	 </form>
+   </header>
